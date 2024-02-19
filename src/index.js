91fbead1edeec631226fba01b35a6c0ef1b799c0
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2024-02-18 22:05:09 -0500

    Added style

--- src/index.js
+++ src/index.js
@@ -41,87 +41,203 @@ onSnapshot(colRef, (snapshot) => {
     });
   });
 
-  console.log(surveyarvid);
-});
-
-// adding documents
-// this collects the data from the form with tag "add"
-const addForm = document.querySelector(".add");
-
-// add an event listener for when the button is pressed
-addForm.addEventListener("submit", (e) => {
-  e.preventDefault(); // preventing default html action when a button is submitted which is refreshing
-
-  addDoc(colRef, {
-    user: addForm.user_id.valueAsNumber,
-    x_value: addForm.x_value.valueAsNumber,
-    y_value: addForm.y_value.valueAsNumber,
-    circle_size: addForm.circle_size.valueAsNumber,
-  }).then(() => {
-    // returns a promise after it is complete
-    addForm.reset(); // reseting values in the form
-  });
-
+  //console.log(surveyarvid);
 });
 
 
 ////////// ChartJS
+const mapOfEvolution = new Map();
+mapOfEvolution.set(0, 'transposable_elments')
+mapOfEvolution.set(1, 'female_meiotic_drive')
+mapOfEvolution.set(2, 'cancer')
+mapOfEvolution.set(3, 'cytoplasmic_elements2')
+mapOfEvolution.set(4, 'genomic_imprinting')
+mapOfEvolution.set(5, 'greenbeard_genes')
+mapOfEvolution.set(6, 'non_selfish_elements')
 
-const data = {
-  //labels: ['Twelve Problems'],
-  //abels: ['January', 'February', 'March', 'April'],
-  datasets: [{
-        type: 'bar',
-        label: 'Bar Dataset',
-        data: [0, 0, 0, 0]
-    }, 
-    {
-        type: 'scatter',
-        label: 'Syntax Difficulty',
-        data: [4.5, 3, 2, 1],
-    },
-    {
-        type: 'scatter',
-        label: 'Conceptual Difficulty',
-        data: [2, 3, 4, 4.5],
-    },
-],
+// Holds whether a value as been paced on the map or not for each
+// selection
+// 0 - hasn't made placement
+// 1 - placed
+var mapOfPlacement = new Map();
+mapOfPlacement.set(0, 1)
+mapOfPlacement.set(1, 1)
+mapOfPlacement.set(2, 1)
+mapOfPlacement.set(3, 1)
+mapOfPlacement.set(4, 1)
+mapOfPlacement.set(5, 1)
+mapOfPlacement.set(6, 0)
+
+var currSelectionKey = 0;
+
+// setup 
+var data = {
+  datasets: []
 };
 
-// config 
-const config = {
-    data,
-    options: {
-        scales: {
-            x: {
-                //type: 'linear',
-            },
-            y: {
-                max:5,
-                ticks: {
-                    stepSize: 1,
-                }
-            }
-        },
+// create buttons
+mapOfEvolution.forEach((values, keys) => {
+  // populate database
+  //console.log(values, keys)
+  data.datasets.push({
+    type: 'scatter',
+    label: values,
+    data: [
+      //{ x: 10, y: 10 }
+    ],
+    pointRadius: 20
+  })
 
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true,
-                }
-            }
-        }
-  }
-};
+  //populate buttons
+  const newButton = document.createElement("button");
+  const buttonText = document.createTextNode(values);
+  newButton.appendChild(buttonText);
+  newButton.id = values;
+  newButton.onclick = () => {
+    //console.log(values)
+    updateBtns(keys)
+  };
 
-// render init block
+  const buttonsDiv = document.getElementById('buttonsDiv')
+  buttonsDiv.appendChild(newButton);
+});
+
+// update buttons according to selection
+updateBtns(currSelectionKey)
+
+function updateBtns(newSelectionKey) {
+  currSelectionKey = newSelectionKey;
+
+  var currSelectedId = mapOfEvolution.get(newSelectionKey)
+  console.log(currSelectedId);
+
+  // set all other button according to their placement
+  mapOfPlacement.forEach((values, keys) => {
+    // placement values 
+    // get its selection value & update visual
+    var currIterationId = mapOfEvolution.get(keys)
+    if (keys == currSelectionKey) {
+      document.getElementById(currIterationId).setAttribute("class", "selected")
+    } else if (values == 0) {
+      document.getElementById(currIterationId).setAttribute("class", "unselected")
+    } else {
+      document.getElementById(currIterationId).setAttribute("class", "added")
+    }
+  })
+}
+
+const font = {
+  size: 20,
+}
+// Render chart inititally
 const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
+  document.getElementById('myChart'), {
+  data,
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Trait distortion',
+          font: font
+        },
+        min: 0,
+        max: 10,
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Transmission distortion',
+          font: font,
+        },
+        max: 10,
+        min: 0,
+      }
+    },
+
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+        }
+      }
+    },
+
+    // When Chart is clicked
+    onClick(e) { chartOnClick(e) },
+  }
+}
 );
 
-function updateChart(barvalue) {
-    console.log(barvalue.value)
-    myChart.config.data.datasets[0].data[0] = barvalue.value;
-    myChart.update();
+// Handles when the chart is clicked
+function chartOnClick(e) {
+  // coordinates of click relative to canvas
+  const { x, y } = Chart.helpers.getRelativePosition(e, myChart);
+  // can also use const x = e.native.offsetX, y = e.native.offsetY;
+
+  // get values relative to chart axes
+  const dataX = Math.round(myChart.scales.x.getValueForPixel(x) * 100) / 100;
+  const dataY = Math.round(myChart.scales.y.getValueForPixel(y) * 100) / 100;
+
+  console.log(dataX, dataY)
+
+  addPoint(dataX, dataY);
+}
+
+// Adds a point to the chart 
+function addPoint(dataX, dataY) {
+  //myChart.config.data.datasets[0].data[0] = barvalue.value;
+
+  // override or add point at click position
+  myChart.config.data.datasets[currSelectionKey].data[0] = { x: dataX, y: dataY };
+  mapOfPlacement.set(currSelectionKey, 1);
+  checkCompletePlacements()
+
+  myChart.update();
+}
+
+function checkCompletePlacements() {
+  console.log('check complete placements')
+
+  // if all the positions are added then we want to add a submit button
+  var boolPlacements = true;
+  mapOfPlacement.forEach((values) => {
+    if (values == 0) {
+      boolPlacements = false;
+    }
+  })
+
+  //console.log(boolPlacements);
+  if (boolPlacements) {
+    document.getElementById('submitBtn').removeAttribute("hidden")
+  }
+}
+
+document.getElementById("submitBtn").addEventListener("click", () => {
+  console.log('submit')
+
+  mapOfEvolution.forEach((values, keys) => {
+    var randomId = getRandomId;
+    var chartDataForKey = myChart.config.data.datasets[keys].data[0];
+    var pointSize = myChart.config.data.datasets[keys].pointRadius;
+    console.log(chartDataForKey, pointSize)
+
+    addDoc(colRef, {
+      // user: addForm.user_id.valueAsNumber,
+      data: chartDataForKey,
+      circle_size: pointSize,
+
+    }).then(() => {
+      // returns a promise after it is complete
+     // addForm.reset(); // reseting values in the form
+    });
+
+
+  })
+}
+);
+
+function getRandomId() {
+  let r = (Math.random() + 1).toString(36).substring(7);
+  return r;
 }
